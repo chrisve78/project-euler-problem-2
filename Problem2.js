165f74c70092a787271c18b15009ccf10112758f
Chris Ellis <cellis8200@gmail.com>
2016-07-16 13:52:55 -0700

    problem sovled

---
+++ Problem2.js
@@ -0,0 +1,21 @@
+var fibFinder = function(value){
+  var firstNum = 1; //first number
+  var secondNum = 0; //second number
+  var nextNum = 0; //next number in sequence
+  var sum = 0; //sum of even number in fib sequence
+
+  while(nextNum <= value){
+    nextNum = secondNum + firstNum; //add first number and second number to get fibonacci sequence - starts with 1
+    if(nextNum % 2 === 0){
+      //test to see if nextNum is even. If so add to sum variable
+      sum += nextNum;
+    }
+    //increment the sequence
+    secondNum = firstNum //assign first number to second number variable
+    firstNum = nextNum; //assign the next number to first number variable
+  }
+
+  return sum;
+};
+
+console.log(fibFinder(4000000))
